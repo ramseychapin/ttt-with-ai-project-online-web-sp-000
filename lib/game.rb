58c79ec85bb2a0133666ed0e0a9b93b5c2541b22
Ramsey Chapin <ramsey@ramseychapin.com>
2019-09-07 23:14:24 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -40,7 +40,33 @@ class Game
     won? || draw? ? true : false
   end
 
-  
+  def winner
+    if won?
+      combination = won?
+      @board.cells[combination[0]] # X or O
+    end
+  end
+
+  def turn
+    puts "Please enter a number 1-9:"
+    @user_input = current_player.move(@board)
+    if @board.valid_move?(@user_input)
+      @board.update(@user_input, current_player)
+    else puts "Please enter a number 1-9:"
+      @board.display
+      turn
+    end
+    @board.display
+  end
+
+  def play
+    turn until over?
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
 
 
 
