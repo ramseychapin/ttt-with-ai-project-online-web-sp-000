8cc1508f284477d703727ecf828f2f564146c8e3
Ramsey Chapin <ramsey@ramseychapin.com>
2019-09-07 23:00:55 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -12,6 +12,13 @@ class game
     [2,4,6]
   ]
 
+  def initialize(player_1=Players::Human.new("X"), player_2=Players::Human.new("O"), board=Board.new)
+    @player_1 = player_1
+    @player_2 = player_2
+    @board = board
+    @board.display
+  end
+
 
 
 end
