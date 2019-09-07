d1cdcdd9067d4bd3c21730aafba7089eebb9e2d0
Ramsey Chapin <ramsey@ramseychapin.com>
2019-09-07 22:44:47 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -36,7 +36,7 @@ class Board
   end
 
   def valid_move?(user_input)
-    user_input.to_i-1 > 0 && user_input.to_i-1 <= 9 && !taken(user_input.to_i-1)
+    user_input > 0 && user_input <= 9 && !taken(user_input)
 
   end
 
