7acc16047bccba77def6b3d6c360d7a1ac70c333
Ramsey Chapin <ramsey@ramseychapin.com>
2019-09-07 23:05:53 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -24,7 +24,12 @@ class game
   end
 
   def won?
-
+    WIN_COMBINATIONS.each do |winning_combination|
+      if (@board[winning_combination[0]] == "X" && @board[winning_combination[1]] == "X" && @board[winning_combination[2]] == "X" || @board[winning_combination[0]] == "O" && @board[winning_combination[1]] == "O" && @board[winning_combination[2]] == "O")
+        return winning_combination
+      end
+    end
+    return false
   end
 
 
